21b9cd97ba638a2edfb48c0ae4ca987ba457a10a
sakshi hase <sakshihase@gmail.com>
2023-03-02 20:52:40 +0530

    initial commit

---
+++ Demo1.java
@@ -0,0 +1,17 @@
+class Demo1
+{
+public static void main(String[] args)
+{
+  String name="Sakshi";
+  byte b=22;
+  char gender='F';
+  long cno=8624889999L;
+  boolean status=false;
+
+System.out.println("name");
+System.out.println(b);
+System.out.println(gender);
+System.out.println(cno);
+System.out.println(status);
+}
+}
